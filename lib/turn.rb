5ce134c7988631bf8d587723aff1db0f3aacb9e5
Abdul <awsayed@alaska.edu>
2019-05-31 03:07:03 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -59,7 +59,7 @@ def turn(board)
   index = input_to_index(input)
   
   if ( valid_move?(board, index) )
-    move(board, index, token = "X")
+    move(board, index)
     display_board(board)
   end
   
